fc80b0f3891abe6428c177c6d73344a41b9cfa0d
techamit95ch <amit98ch@gmail.com>
2021-07-19 14:45:10 +0530

    Error Handelling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 const SearchScreen = () => {
-    const [term, setTerm] = React.useState('');
-    const [results, setResults] = React.useState([]);
-    const searchApi = async () =>{
-        try {
-            const response = await yelp.get('/search',{
-                params: {
-                    limit:50,
-                    term,
-                    location:"san jose"
-                }
-            });
-            setResults(response.data.businesses);
-            console.log(response.data.businesses);
-        }catch(err) {console.log(error.message);}
+  const [term, setTerm] = React.useState("");
+  const [results, setResults] = React.useState([]);
+  const [error, setError] = React.useState("");
+  const searchApi = async (SearchTearm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term:SearchTearm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+      console.log(response.data.businesses);
+    } catch (err) {
+      console.log(err);
+      setError("Something went wrong");
     }
-    return (
-        <View>
-            <SearchBar term={term} onChangeTerm={setTerm} onTermSubmit = {searchApi}/>
-            <Text>Search Text => {term}</Text>
-            <Text>we have found {results.length} results</Text>
-        </View>
-    )
-}
+  };
+  return (
+    <View>
+      <SearchBar term={term} onChangeTerm={setTerm} onTermSubmit={()=>searchApi(term)} />
+      {error !=="" ? <Text>{error}</Text> : null}
+      <Text>Search Text : {term}</Text>
+      <Text>we have found {results.length} results</Text>
+    </View>
+  );
+};
 
-export default SearchScreen
+export default SearchScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
